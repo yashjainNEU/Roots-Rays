ac221e0e4c564e62bb20963c3c2b335d0461417c
Yash Jain NUID(002871982) <jain.yash8@northeastern.edu>
2024-09-24 20:24:54 -0400

    LeafyLane Website

---
+++ main.js
@@ -0,0 +1,44 @@
+// navigation menu
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector("#hamburger");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("left-[0]");
+  hamburger.classList.toggle("ri-close-large-line");
+});
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]");
+    hamburger.classList.toggle("ri-close-large-line");
+  });
+});
+
+// swiper JS
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
