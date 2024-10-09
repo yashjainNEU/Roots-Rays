4069a84e7c6148b47436add334caa68fac9c4610
Yash Jain NUID(002871982) <jain.yash8@northeastern.edu>
2024-10-08 23:02:18 -0400

    updated content

--- main.js
+++ main.js
@@ -42,3 +42,27 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+// scroll reveal
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 300,
+  reset: true,
+});
+sr.reveal(
+  `.home__data, .about__top, .popular__top, .reviev__top,.review__swiper,.footer__social_icon,.footer__bottom,.footer__copyright`
+);
+sr.reveal(`.home__img`, { delay: 500, scale: 0.5 });
+sr.reveal(`.service__card,.popular__card`, { interval: 200 });
+sr.reveal(`.about__img_leaf`, { delay: 600, origin: "right" });
+sr.reveal(`.img__leaf_2,.footer__floral_img`, { delay: 600, origin: "left" });
+sr.reveal(`.about__content_1, .about__image_2`, {
+  delay: 600,
+  origin: "left",
+});
+sr.reveal(`.about__content_2, .about__image_1`, {
+  delay: 600,
+  origin: "right",
+});
